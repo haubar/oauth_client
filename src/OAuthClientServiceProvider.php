a4719fee69e61fcadd74e8de6fea04e06d28c802
haubar <haubarclack@gmail.com>
2015-03-24 18:00:32 +0800

    oauth client reset

---
+++ src/OAuthClientServiceProvider.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ *
+ * @package   ithome/
+ * @author    Clark <clark@mail.ithome.com.tw>
+ * @copyright Copyright (c) Clark
+ * @licence   http://mit-license.org/
+ */
+
+namespace Ithome\OAuthClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class OAuthClientServiceProvider extends ServiceProvider
+{
+    /**
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('ithome/oauth-client');
+
+        // $this->bootFilters();
+    }
+
+    /**
+     * Register the service provider.
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerCommands();
+        $this->registerErrorHandlers();
+    }
+
+
+    /**
+     * Get the services provided by the provider.
+     * @return string[]
+     * @codeCoverageIgnore
+     */
+    public function provides()
+    {
+        return [];
+    }
+
+    /**
+     * Registers some utility commands with artisan
+     * @return void
+     */
+    public function registerCommands()
+    {
+
+        //定義指令檔的動作
+        $this->app->bind('command.oauth-client.controller', 'Ithome\OAuthClient\Console\OAuthControllerCommand');
+        $this->app->bind('command.oauth-client.migrations', 'Ithome\OAuthClient\Console\MigrationsCommand');
+        $this->app->bind('command.oauth-client:set-consumer', 'Ithome\OAuthClient\Console\ClientCreatorCommand');
+
+        //action 指令
+        $this->commands(
+                        'command.oauth-client.controller',
+                        'command.oauth-client.migrations',
+                        'command.oauth-client:set-consumer'
+                        );
+
+
+        // $this->app->bind('command.oauth-client.controller', 'Ithome\OAuthClient\Console\OAuthControllerCommand');
+        // $this->app->bind('command.oauth-client.migrations', 'Ithome\OAuthClient\Console\MigrationsCommand');
+        // $this->app->bind('command.oauth-client.client.create', 'Ithome\OAuthClient\Console\ClientCreatorCommand');
+
+        // $this->commands('command.oauth-client.controller', 'command.oauth-client.migrations', 'command.oauth-client.client.create');
+    }
+
+    /**
+     * Boot the filters
+     * @return void
+     */
+    private function bootFilters()
+    {
+        // $this->app['router']->filter('check-authorization-params', 'LucaDegasperi\OAuth2client\Filters\CheckAuthCodeRequestFilter');
+        // $this->app['router']->filter('oauth', 'LucaDegasperi\OAuth2client\Filters\OAuthFilter');
+        // $this->app['router']->filter('oauth-owner', 'LucaDegasperi\OAuth2client\Filters\OAuthOwnerFilter');
+    }
+
+    /**
+     * Register the OAuth error handlers
+     * @return void
+     */
+    private function registerErrorHandlers()
+    {
+        $this->app->error(function(OAuthException $e) {
+            if($e->shouldRedirect()) {
+                return new RedirectResponse($e->getRedirectUri());
+            } else {
+                return new JsonResponse([
+                                'error' => $e->errorType,
+                                'error_description' => $e->getMessage()
+                        ],
+                        $e->httpStatusCode,
+                        $e->getHttpHeaders()
+                );
+            }
+        });
+    }
+}
