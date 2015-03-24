a445104167be836e22b59d20fa17ebab76b3f08a
haubar <haubarclack@gmail.com>
2015-03-25 00:00:51 +0800

    update

---
+++ src/Ithome/OAuthClient/Facade/OAuthClient.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author     Dariusz Prząda <artdarek@gmail.com>
+ * @copyright  Copyright (c) 2013
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+
+namespace Ithome\OAuthClient\Facade;
+
+use Illuminate\Support\Facades\Facade;
+
+class OAuthClient extends Facade
+{
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'oauth-client';
+    }
+
+}
